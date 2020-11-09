d2321bb8888e9dc70f9403649fc9d6946f379c5a
Waddles <180110515@stu.hit.edu.cn>
2020-11-09 14:30:26 +0800

    change pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -9,23 +9,21 @@ main(int argc, char *argv[])
 	int parent_p[2], child_p[2];
 	pipe(child_p);
 	pipe(parent_p);
-	char buf[4];
+	char buf[10];
 	
 	if (fork() == 0) {
 		close(parent_p[W]);
 		close(child_p[R]);
 		read(parent_p[R], &buf, 4);
 		printf("%d: received %s\n", getpid(), buf);
-		buf = "pong"
-		write(child_p[W], &buf, 4);
+		write(child_p[W], "pong", 4);
 		close(parent_p[R]);
 		close(child_p[W]);
 	} 
 	else {
 		close(parent_p[R]);
 		close(child_p[W]);
-		buf = "ping"
-		write(parent_p[W], &buf, 4);
+		write(parent_p[W], "ping", 4);
 		read(child_p[R], &buf, 4);
 		printf("%d: received %s\n", getpid(), buf);
 		close(parent_p[W]);
