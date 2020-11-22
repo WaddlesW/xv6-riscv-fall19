9679680f0df43a002f78e4ab0f7e02906fe142c9
Waddles <180110515@stu.hit.edu.cn>
2020-11-22 22:26:08 +0800

    lab2v3.2

--- user/nsh.c
+++ user/nsh.c
@@ -1,107 +1,146 @@
 #include "kernel/types.h"
+#include "kernel/stat.h"
 #include "user/user.h"
 #include "kernel/fcntl.h"
- 
-void execPipe(char*argv[],int argc); 
- 
-#define MAXARGS 32
-#define MAXCHAR 128
+
+#define MAX_CHAR 128
+#define MAX_ARGS 32
 #define R 0
 #define W 1
- 
-int getcmd(char *buf, int nbuf)
-{
-    fprintf(2, "@ ");
-    memset(buf, 0, nbuf);
-    gets(buf, nbuf);
-    if (buf[0] == 0) // EOF
-        return -1;
-    return 0;
+
+int
+readline(char* buf, int n) {
+	gets(buf, n);
+	if (buf[0] == 0) return -1;
+	buf[strlen(buf) - 1] = 0;
+	return 0;
 }
 
-char args[MAXARGS][MAXCHAR];
+int open_stdin(char* path) {
+	close(0);
+	if (open(path, O_RDONLY) != 0) {
+		fprintf(2, "open stdin %s failed!\n", path);
+		exit(1);
+	}
+	return 0;
+}
 
-void setargs(char *cmd, char* argv[],int* argc)
-{
-	int i = 0;
-    for(;i<MAXARGS;i++){
-        argv[i]=&args[i][0];
-    }
+int redirect_stdin(int fd) {
+	close(0);
+	if (dup(fd) != 0) {
+		fprintf(2, "redirect stdin failed!\n");
+		exit(1);
+	}
+	return 0;
+}
 
-    for (int j=0, i=0; cmd[j] != '\n' && cmd[j] != '\0'; j++)
-    {
-        while (strchr(" \t\r\n\v",cmd[j])){
-            j++;
-        }
-        argv[i++]=cmd+j;
-        while (strchr(" \t\r\n\v",cmd[j])==0){
-            j++;
-        }
-        cmd[j]='\0';
-    }
-    argv[i]=0;
-    *argc=i;
+int open_stdout(char* path) {
+	close(1);
+	if (open(path, O_CREATE | O_WRONLY) != 1) {
+		fprintf(2, "open stdout %s failed!\n", path);
+		exit(1);
+	}
+	return 0;
 }
- 
-void runcmd(char*argv[],int argc)
+
+int redirect_stdout(int fd) {
+	close(1);
+	if (dup(fd) != 1) {
+		fprintf(2, "redirect stdout failed!\n");
+		exit(1);
+	}
+	return 0;
+}
+
+int destroy_stdin() {
+	close(0);
+	return 0;
+}
+
+int destroy_stdout() {
+	close(1);
+	return 0;
+}
+
+
+int
+run(char* path, char** argv) {
+	char** pipe_argv = 0;
+	char* stdin = 0;
+	char* stdout = 0;
+	for (char** v = argv; *v != 0; ++v) {
+		if (strcmp(*v, "<") == 0) {
+			*v = 0;
+			stdin = *(v + 1);
+			++v;
+		}
+		if (strcmp(*v, ">") == 0) {
+			*v = 0;
+			stdout = *(v + 1);
+			++v;
+		}
+		if (strcmp(*v, "|") == 0) {
+			*v = 0;
+			pipe_argv = v + 1;
+			break;
+		}
+	}
+
+	if (fork() == 0) {
+		int fd[2];
+		if (pipe_argv != 0) {
+			pipe(fd);
+			if (fork() == 0) {
+				close(fd[W]);
+				redirect_stdin(fd[R]);
+				run(pipe_argv[0], pipe_argv);
+				close(fd[R]);
+				destroy_stdin();
+				exit(0);
+			}
+			close(fd[R]);
+			redirect_stdout(fd[W]);
+		}
+
+		if (stdin != 0) open_stdin(stdin);
+		if (stdout != 0) open_stdout(stdout);
+    
+		exec(path, argv);
+
+		if (stdin != 0) destroy_stdin(stdin);
+		if (stdout != 0) destroy_stdout(stdout);
+  
+		if (pipe_argv != 0) {
+			close(fd[W]);
+			destroy_stdout();
+			wait(0);
+		}
+		exit(0);
+	} else {
+		wait(0);
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[])
 {
-    for(int i=1;i<argc;i++){
-        if(!strcmp(argv[i],"|")){
-            execPipe(argv,argc);
-        }
-    }
-    for(int i=1;i<argc;i++){
-        if(!strcmp(argv[i],">")){
-            close(1);
-            open(argv[i+1],O_CREATE|O_WRONLY);
-            argv[i]=0;
-        }
-        if(!strcmp(argv[i],"<")){
-            close(0);
-            open(argv[i+1],O_RDONLY);
-            argv[i]=0;
-        }
-    }
-    exec(argv[0], argv);
-}
- 
-void execPipe(char*argv[],int argc){
-    int i=0;
-	for(;i<argc;i++){
-        if(!strcmp(argv[i],"|")){
-            argv[i]=0;
-            break;
-        }
-    }
-    int fd[2];
-    pipe(fd);
-    if(fork()==0){	// 子进程 执行左边的命令 把自己的标准输出关闭
-        close(1);
-        dup(fd[W]);
-        close(fd[R]);
-        close(fd[W]);
-        runcmd(argv,i);
-    }else{	// 父进程 执行右边的命令 把自己的标准输入关闭
-        close(0);
-        dup(fd[R]);
-        close(fd[R]);
-        close(fd[W]);
-        runcmd(argv+i+1,argc-i-1);
-    }
-}
-int main()
-{
-    char buf[MAXCHAR];
-    while (getcmd(buf, sizeof(buf)) >= 0)
-    {
-        if (fork() == 0)
-        {
-            char* argv[MAXARGS];
-            int argc=-1;
-            setargs(buf, argv,&argc);
-            runcmd(argv,argc);
-        }
-        wait(0);
-    }
-    exit(0);
+	char buf[MAX_CHAR] = { 0 };
+	char* aargv[MAX_ARGS] = { 0 };
+	int aargc = 0;
+	printf("@ ");
+	while(!readline(buf, MAX_CHAR)) {
+		int _len = strlen(buf);
+		aargc = 0;
+		aargv[aargc++] = buf;
+		for (int i = 0; i < _len; i++) {
+			if (buf[i] == ' ') {
+				buf[i] = '\0';
+				aargv[aargc++] = &buf[i + 1];
+			}
+		}
+		aargv[aargc] = 0;
+		run(aargv[0], aargv);
+		printf("@ ");
+	}
+	exit(0);
 }
