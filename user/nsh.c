b50023dad2612b7e3b57caa0df0ad4717b22f7d5
Waddles <180110515@stu.hit.edu.cn>
2020-11-22 23:13:40 +0800

    lab2v4.0

--- user/nsh.c
+++ user/nsh.c
@@ -3,23 +3,10 @@
 #include "user/user.h"
 #include "kernel/fcntl.h"
 
-#define MAX_CHAR 128
-#define MAX_ARGS 32
-#define R 0
-#define W 1
-
-int
-readline(char* buf, int n) {
-	gets(buf, n);
-	if (buf[0] == 0) return -1;
-	buf[strlen(buf) - 1] = 0;
-	return 0;
-}
-
 int open_stdin(char* path) {
 	close(0);
 	if (open(path, O_RDONLY) != 0) {
-		fprintf(2, "open stdin %s failed!\n", path);
+		printf("open stdin %s failed!\n", path);
 		exit(1);
 	}
 	return 0;
@@ -28,7 +15,7 @@ int open_stdin(char* path) {
 int redirect_stdin(int fd) {
 	close(0);
 	if (dup(fd) != 0) {
-		fprintf(2, "redirect stdin failed!\n");
+		printf("redirect stdin failed!\n");
 		exit(1);
 	}
 	return 0;
@@ -37,7 +24,7 @@ int redirect_stdin(int fd) {
 int open_stdout(char* path) {
 	close(1);
 	if (open(path, O_CREATE | O_WRONLY) != 1) {
-		fprintf(2, "open stdout %s failed!\n", path);
+		printf("open stdout %s failed!\n", path);
 		exit(1);
 	}
 	return 0;
@@ -46,38 +33,24 @@ int open_stdout(char* path) {
 int redirect_stdout(int fd) {
 	close(1);
 	if (dup(fd) != 1) {
-		fprintf(2, "redirect stdout failed!\n");
+		printf("redirect stdout failed!\n");
 		exit(1);
 	}
 	return 0;
 }
 
-int destroy_stdin() {
-	close(0);
-	return 0;
-}
-
-int destroy_stdout() {
-	close(1);
-	return 0;
-}
-
-
-int
-run(char* path, char** argv) {
+int run(char* path, char** argv) {
 	char** pipe_argv = 0;
 	char* stdin = 0;
 	char* stdout = 0;
 	for (char** v = argv; *v != 0; ++v) {
 		if (strcmp(*v, "<") == 0) {
 			*v = 0;
-			stdin = *(v + 1);
-			++v;
+			stdin = *(++v);
 		}
 		if (strcmp(*v, ">") == 0) {
 			*v = 0;
-			stdout = *(v + 1);
-			++v;
+			stdout = *(++v);
 		}
 		if (strcmp(*v, "|") == 0) {
 			*v = 0;
@@ -91,15 +64,15 @@ run(char* path, char** argv) {
 		if (pipe_argv != 0) {
 			pipe(fd);
 			if (fork() == 0) {
-				close(fd[W]);
-				redirect_stdin(fd[R]);
+				close(fd[1]);
+				redirect_stdin(fd[0]);
 				run(pipe_argv[0], pipe_argv);
-				close(fd[R]);
-				destroy_stdin();
+				close(fd[0]);
+				close(0);
 				exit(0);
 			}
-			close(fd[R]);
-			redirect_stdout(fd[W]);
+			close(fd[0]);
+			redirect_stdout(fd[1]);
 		}
 
 		if (stdin != 0) open_stdin(stdin);
@@ -107,12 +80,12 @@ run(char* path, char** argv) {
     
 		exec(path, argv);
 
-		if (stdin != 0) destroy_stdin(stdin);
-		if (stdout != 0) destroy_stdout(stdout);
+		if (stdin != 0) close(0);;
+		if (stdout != 0) close(1);;
   
 		if (pipe_argv != 0) {
 			close(fd[W]);
-			destroy_stdout();
+			close(1);;
 			wait(0);
 		}
 		exit(0);
@@ -122,17 +95,24 @@ run(char* path, char** argv) {
 	return 0;
 }
 
+int readline(char* buf, int n) {
+	gets(buf, n);
+	if (buf[0] == 0) return -1;
+	buf[strlen(buf) - 1] = 0;
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
-	char buf[MAX_CHAR] = { 0 };
-	char* aargv[MAX_ARGS] = { 0 };
+	char buf[128] = { 0 };
+	char* aargv[32] = { 0 };
 	int aargc = 0;
 	printf("@ ");
-	while(!readline(buf, MAX_CHAR)) {
-		int _len = strlen(buf);
+	while(!readline(buf, 128)) {
+		int buf_len = strlen(buf);
 		aargc = 0;
 		aargv[aargc++] = buf;
-		for (int i = 0; i < _len; i++) {
+		for (int i = 0; i < buf_len; i++) {
 			if (buf[i] == ' ') {
 				buf[i] = '\0';
 				aargv[aargc++] = &buf[i + 1];
